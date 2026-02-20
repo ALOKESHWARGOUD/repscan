f913dca3df148b036908500c880c7b74ef414f9d
LOKESHWARGOUD <131950789+LOKESHWARGOUD@users.noreply.github.com>
2026-02-20 16:52:04 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5174,        // match your current port
-    allowedHosts: 'all'
-  }
 })
